2a48a38867cca4511a10533982d3827a22fa5e96
NightVanHelsing <99742178+NightVanHelsing@users.noreply.github.com>
2026-08-28 16:43:12 +0200

    Bump version to 1.1.0 and include popupsource.php
    Updated plugin version to 1.1.0 and added popupsource.php include.

--- anise-simple-discounts/anise-simple-discounts.php
+++ anise-simple-discounts/anise-simple-discounts.php
@@ -2,10 +2,9 @@
 /*
  * Plugin Name: Anise Simple Discounts
  * Text Domain: anise-simple-discounts
- * Domain Path: /languages
  * Author: Michał Adamek
  * Author URI: https://github.com/NightVanHelsing
- * Version: 1.0.0
+ * Version: 1.1.0
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Requires Plugins: woocommerce
@@ -15,5 +14,7 @@
 if (!defined('ABSPATH')){
 	exit;
 }
+define('ASD_PLUGIN_URL',plugin_dir_url(__FILE__));
 require_once __DIR__.'/include/discounts.php';  
 require_once __DIR__.'/include/user-panel.php';
+require_once __DIR__.'/include/popupsource.php';
